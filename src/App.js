e8a2aa166743a9b9202a6c6215e436b386ca89d4
Vaibhav Kumar <vaibhavkr87097@gmail.com>
2023-09-21 00:59:27 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/Navbar";
+import footer from "./components/footer/footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <div>
+      hfjewifloierftu34
+    </div>
+  )
+}
+
+export default footer
